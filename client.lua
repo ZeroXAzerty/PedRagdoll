88dccb96b52ea61505efcdf309ab54258f89775c
ZeroXAzerty <87154152+ZeroXAzerty@users.noreply.github.com>
2021-11-06 04:32:30 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -7,7 +7,7 @@ Citizen.CreateThread(function()
             while run == true do
                 Citizen.Wait(0)
 
-                if IsControlJustPressed(1, 344) then 
+                if IsControlJustPressed(1, 344) then # 344 = F11
                     run = false
                 else
                     SetPedToRagdoll(PlayerPedId(), 1000, 1000, 0, true, true , false)
