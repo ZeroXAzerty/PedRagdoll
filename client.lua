6f5bfd87b8e16882637c5a7054094c402db35f6f
ZeroXAzerty <87154152+ZeroXAzerty@users.noreply.github.com>
2021-11-06 04:31:59 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,20 @@
+Citizen.CreateThread(function()
+    local run = false
+    while true do
+        Citizen.Wait(0)
+        if IsControlJustPressed(1, 344) then 
+            run = true
+            while run == true do
+                Citizen.Wait(0)
+
+                if IsControlJustPressed(1, 344) then 
+                    run = false
+                else
+                    SetPedToRagdoll(PlayerPedId(), 1000, 1000, 0, true, true , false)
+                end
+            end
+        end
+    end
+
+
+end)
